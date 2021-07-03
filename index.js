f144f555338a9170cf88b955dd787f6ffaba15b7
primocodetoday <primocodetoday@gmail.com>
2021-07-03 21:14:28 +0200

    dev: router added

--- index.js
+++ index.js
@@ -1,31 +1,19 @@
 ﻿import express from 'express';
 import path from 'path';
 import hogan from 'hogan-middleware';
-
-const __dirname = path.resolve();
+import router from './routes/index.js';
 
 const app = express();
+const __dirname = path.resolve();
+const port = 3000;
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'mustache');
 app.engine('mustache', hogan.__express);
+app.use(express.static(path.join(__dirname, 'public')));
 
-const port = 3000;
-
-app.get('/', (req, res, next) => {
-  res.send('Hello World');
-});
-
-app.get('/json', (req, res, next) => {
-  const data = {
-    greeting: 'Hello',
-  };
-  res.send(data);
-});
-
-app.get('/home', (req, res, next) => {
-  res.render('home', null);
-});
+app.use('/', router);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
---
+++ routes/index.js
@@ -0,0 +1,20 @@
+﻿import { Router } from 'express';
+
+const router = Router();
+
+router.get('/', (req, res, next) => {
+  res.send('Hello World');
+});
+
+router.get('/json', (req, res, next) => {
+  const data = {
+    greeting: 'Hello',
+  };
+  res.send(data);
+});
+
+router.get('/home', (req, res, next) => {
+  res.render('home', null);
+});
+
+export default router;

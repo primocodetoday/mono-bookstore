ea2e39cfca514a04d88465168b65391f073bc0c2
primocodetoday <primocodetoday@gmail.com>
2021-07-06 22:55:43 +0200

    dev: CRUD for books created

---
+++ models/book.js
@@ -0,0 +1,27 @@
+﻿const mongoose = require('mongoose');
+
+const BookSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  created_at: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model('Book', BookSchema);
--- routes/api.js
+++ routes/api.js
@@ -1,23 +1,78 @@
 // Full Documentation - https://www.turbo360.co/docs
 const express = require('express');
-
 const router = express.Router();
+const Book = require('../models/book');
 
-router.get('/:resource', (req, res) => {
-  res.json({
-    confirmation: 'success',
-    resource: req.params.resource,
-    query: req.query, // from the url query string
-  });
+const getBook = async (req, res, next) => {
+  let book;
+
+  try {
+    book = await Book.findById(req.params.id);
+    if (book === null) {
+      return res.status(404).json({ message: 'Cannot find book' });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+
+  res.book = book;
+  next();
+};
+
+// Getting all
+router.get('/', async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.json(books);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
-router.get('/:resource/:id', (req, res) => {
-  res.json({
-    confirmation: 'success',
-    resource: req.params.resource,
-    id: req.params.id,
-    query: req.query, // from the url query string
+// Getting one
+router.get('/:id', getBook, (req, res) => {
+  res.send(res.book);
+});
+
+// Store
+router.post('/', async (req, res) => {
+  const book = new Book({
+    name: req.body.name,
+    author: req.body.author,
+    description: req.body.description,
+    price: req.body.price,
   });
+
+  try {
+    const newBook = await book.save();
+    res.status(201).json(newBook);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Update
+router.patch('/:id', getBook, async (req, res) => {
+  if (req.body.name !== null) {
+    res.book.name = req.body.name;
+  }
+
+  try {
+    const updatedBook = await res.book.save();
+    res.json(updatedBook);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Delete
+router.delete('/:id', getBook, async (req, res) => {
+  try {
+    await res.book.remove();
+    res.json({ message: `Deleted book - ${res.book.name}` });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 module.exports = router;

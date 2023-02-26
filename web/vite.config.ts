e8a947b7168ccc1558911686bebb41a322fa5ecd
primocodetoday <primocodetoday@gmail.com>
2023-02-26 20:02:03 +0100

    web: vite added

---
+++ web/vite.config.ts
@@ -0,0 +1,19 @@
+﻿import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
+export default ({mode}) => {
+	process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+	return defineConfig({
+		plugins: [react()],
+		server:{
+			port: parseInt(process.env.VITE_DEV_PORT)
+		},
+		resolve:{
+			alias:{
+				'@' : resolve(__dirname, './src')
+			},
+		},
+	})
+}

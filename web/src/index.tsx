297e818fa18a7645df1a7a45ef0eabcf57dd1ddb
primocodetoday <primocodetoday@gmail.com>
2023-02-26 18:50:47 +0100

    web: router handler changed

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client";
 import App from './App';
 import './i18n';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>,
-	document.getElementById('root'),
 );

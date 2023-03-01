7d45f7244e568e8d46065752ff8c756491feea07
primocodetoday <primocodetoday@gmail.com>
2023-03-01 20:43:39 +0100

    api: User + Role model

---
+++ api/models/Role.model.js
@@ -0,0 +1,9 @@
+﻿const mongoose = require('mongoose');
+
+const Role = new mongoose.Schema({
+    name: {
+        type: String,
+    },
+});
+
+module.exports = mongoose.model('Role', Role);
---
+++ api/models/User.model.js
@@ -0,0 +1,24 @@
+﻿const mongoose = require('mongoose');
+
+const User = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    roles: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Role',
+        },
+    ],
+});
+
+module.exports = mongoose.model('User', User);
